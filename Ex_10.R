c1b59864471ab027172fbfb157b78d803f975e70
AlexDosch1 <91433155+AlexDosch1@users.noreply.github.com>
2021-11-26 10:19:54 -0500

    Final Final Changes

--- Ex_10.R
+++ Ex_10.R
@@ -30,7 +30,7 @@ obs <- read.table("data.txt", sep = ",", header = TRUE, stringsAsFactors = TRUE)
 library(ggplot2)
 library(cowplot)
 
-# show barplot of means of four populations, store as plot1
+# show bar plot of means of four populations, store as plot1
   # Calculate means for each region
 mean_north <- mean(obs[1:1000, 2])
 mean_east <- mean(obs[1001:2000, 2])
@@ -40,8 +40,8 @@ mean_west <- mean(obs[3001:4000, 2])
 mean_obs <- data.frame(region=c("north", "east", "south","west"),
            mean=c(mean_north, mean_east, mean_south, mean_west))
 
-plot1 <- ggplot(data = mean_obs, aes(x = region, fill = as.factor(mean))) +
-  geom_bar() +
+plot1 <- ggplot(data = mean_obs, aes( x = region, y=mean)) +
+  geom_col() +
   theme_bw() +
   xlab("Region") +
   ylab("Mean Observations")
@@ -67,6 +67,6 @@ plot_grid(plot1, plot2,
   # trend lines and relationship patterns while bar plots give visual 
   # descriptions of categorical trends or properties. In this case, they tell 
   # similar stories due to the categorical nature of the data, however the bar 
-  # plot shows the exact mean of the data while the scatter plot shows the range 
-  # of observations for each region, with the dark clusters indicating a vague
-  # mean value rather than an exact one. 
+  # plot shows the exact mean of the data, not taking into account high or low 
+  # clusters of data, while the scatter plot shows the range of observations
+  # for each region, with the dark clusters indicating high concentrations of data. 
