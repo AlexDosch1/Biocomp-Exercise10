eefbec31521c0492ab1b7981931e6b2d0c25078c
AlexDosch1 <91433155+AlexDosch1@users.noreply.github.com>
2021-11-26 00:47:24 -0500

    Challenge 1 first edits

---
+++ Ex_10.R
@@ -0,0 +1,10 @@
+# Challenge 1: 
+
+#Find data with two related variables and save into text file and load data
+V_vs_T<-read.csv("Lab_4.2 copy.csv", header = TRUE, stringsAsFactors = FALSE)
+
+#Produce scatter plot with trendline
+
+library(ggplot2)
+
+
